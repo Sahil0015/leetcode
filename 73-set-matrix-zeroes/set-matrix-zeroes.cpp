6bd436c190c1e59ae8332e3d86fe9d4d13ea0ee2
Sahil Aggarwal <124445596+Sahil0015@users.noreply.github.com>
2025-11-28 12:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (11.16%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-
         int n = mat.size();
+        if (n == 0) return;
         int m = mat[0].size();
+        vector<vector<int>> zeroes;
 
-        vector<vector<int>> answer;
-
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n ;i++) {
             vector<int> row;
             row.push_back(i);
-            for(int j=0; j<m; j++) {
-                if(mat[i][j] == 0) {
+            for(int j=0; j<m; j++)
+                if(mat[i][j] == 0)
                     row.push_back(j);
-                }
-            }
-            answer.push_back(row);
+            zeroes.push_back(row);
         }
 
-        for(int i=0; i<answer.size(); i++) {
-            if(answer[i].size() == 1) continue;
-            else {
-                int len = answer[i].size();
+        for(int i=0; i<n ;i++) {
+            if(zeroes[i].size() == 1) continue;
+            for(int j=1; j<zeroes[i].size(); j++) {
+                for(int k=0; k<n; k++) 
+                    mat[k][zeroes[i][j]] =0;
 
-                for(int k=0; k<m; k++) {
-                    mat[answer[i][0]][k] = 0;
-                }
-
-                for(int j=1; j<len; j++) {
-                    for(int k=0; k<n; k++) {
-                        mat[k][answer[i][j]] = 0;
-                    }
-                }
+                for(int k=0; k<m; k++) 
+                    mat[zeroes[i][0]][k] = 0;
             }
-        }
+      }
+
+      return;
     }
 };
